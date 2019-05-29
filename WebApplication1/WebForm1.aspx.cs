7160924c6e2f4b73fced88fdfddfc7e60b8227f9
Juan Carlos <juancalz53@gmail.com>
2019-05-28 22:45:35 -0600

    primer commit

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btn.Style.Add("visibility", "hidden");
+                
+            }
+
+            if (IsPostBack && FileUpload1.PostedFile != null)
+            {
+
+                if (FileUpload1.PostedFile.FileName.Length > 0)
+                {
+                    btn.Style["visibility"] = "visible";
+                }
+                else
+                {
+                    btn.Style["visibility"] = "hidden";
+                }
+
+                
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
